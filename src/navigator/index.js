c4aeb70a86a1ada13c1c4a9542a0c9d3881da276
amyusup <amyusup26@gmail.com>
2020-11-15 20:40:26 +0700

    local notification

--- src/navigator/index.js
+++ src/navigator/index.js
@@ -24,11 +24,43 @@ import {
 import {useSelector} from 'react-redux';
 import SplashScreen from 'react-native-splash-screen';
 import color from '../styles/constant'
+import messaging from '@react-native-firebase/messaging';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
 const AuthStack = () => {
+  const [initialRoute, setInitialRoute] = React.useState('Main');
+  const [loading, setLoading] = React.useState(true);
+  React.useEffect(() => {
+    // Assume a message-notification contains a "type" property in the data payload of the screen to open
+
+    messaging().onNotificationOpenedApp(remoteMessage => {
+      console.log(
+        'Notification caused app to open from background state:',
+        remoteMessage.notification,
+      );
+      navigation.navigate(remoteMessage.data.type);
+    });
+
+    // Check whether an initial notification is available
+    messaging()
+      .getInitialNotification()
+      .then(remoteMessage => {
+        if (remoteMessage) {
+          console.log(
+            'Notification caused app to open from quit state:',
+            remoteMessage.notification,
+          );
+          setInitialRoute(initialRoute); // e.g. "Settings"
+        }
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return null;
+  }
   return (
     <Stack.Navigator>
       <Stack.Screen name="SignIn" component={SignIn} options={{headerShown: false}}
