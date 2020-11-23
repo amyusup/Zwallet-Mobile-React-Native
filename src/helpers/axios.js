8c7b9c659d331cb9b280dcfce53b6c934d6ae873
amyusup <amyusup26@gmail.com>
2020-11-23 23:04:50 +0700

    setup axios deploy

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   // baseURL: "http://54.86.186.28:4444/api/v1",
   // baseURL: "http://localhost:4444/api/v1",
-  baseURL: "http://192.168.43.149:4444/api/v1",
-  // baseURL: "https://zwallet-team.herokuapp.com/api/v1",
+  // baseURL: "http://192.168.43.149:4444/api/v1", // local
+  baseURL: "https://zwallet-team.herokuapp.com/api/v1", // deploy
 });
